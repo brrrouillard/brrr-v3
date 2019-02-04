82cbdde0616013bb532269b6bea6d35de8979c8a
Matthieu Brouillard <mbrouilla@gmail.com>
2019-02-04 12:01:05 +0100

    Updates

---
+++ precache-manifest.b807c21939b894cbac350567018c759e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "82fdb3ead296cd65d40a",
+    "url": "/brrr-v3/static/js/runtime~main.82fdb3ea.js"
+  },
+  {
+    "revision": "5009457ac1054cbc9046",
+    "url": "/brrr-v3/static/js/main.5009457a.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/brrr-v3/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "5009457ac1054cbc9046",
+    "url": "/brrr-v3/static/css/main.8c92c52f.chunk.css"
+  },
+  {
+    "revision": "40140001552eca81b2bcf05136dd3b0d",
+    "url": "/brrr-v3/index.html"
+  }
+];
