be44b4c5e1037bab057fceaca7689fd049d7de05
Matthieu Brouillard <mbrouilla@gmail.com>
2019-02-04 12:29:20 +0100

    Add animations

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -6,11 +6,11 @@ export class Landing extends Component {
   render() {
     return (
       <div id="Landing-Section">
-        <h1>Matthieu Brouillard</h1>
-        <h3>Full-Stack Web Developer</h3>
-        <div id="Social-Container">
-          <a href="https://github.com/brrrouillard"><i className="fab fa-github" /></a>
-          <a href="www.linkedin.com/in/matthieu-brouillard"><i class="fab fa-linkedin" /></a>
+        <h1 className="animated fadeInUp">Matthieu Brouillard</h1>
+        <h3 className="animated fadeInUp delay-1s">Full-Stack Web Developer</h3>
+        <div className="animated fadeIn" id="Social-Container">
+          <a href="https://github.com/brrrouillard" target="_blank"><i className="fab fa-github" /></a>
+          <a href="www.linkedin.com/in/matthieu-brouillard" target="_blank"><i class="fab fa-linkedin" /></a>
           <a href="mailto:mbrouilla@gmail.com"><i class="fas fa-envelope"></i></a>
         </div>
       </div>
